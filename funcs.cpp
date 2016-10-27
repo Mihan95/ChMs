46f48c01eda4c7f43e819004440ddabcecf5a43e
Кирилин Михаил Олегович <Kirilin_MO@student.math.msu.su>
2016-10-27 15:33:10 +0300

    some ready

--- funcs.cpp
+++ funcs.cpp
@@ -111,34 +111,34 @@ double f(double t, double x)
     return (- ro(t, x) * sin(M_PI*x*x*0.01) * sin(M_TWOPI*t) * M_TWOPI
            + ro(t, x) * u(t, x) * cos(M_TWOPI*t) * cos(M_PI*x*x*0.01) * M_PI * x / 50.0
            - 1.4 * M_PI * 0.1 * exp(1.4*t) * sin(M_PI*x*0.1) * (cos(M_PI*x*0.1) + 1.5)
-           - MY * cos(M_TWOPI*t) * M_PI * 0.02 * (cos(M_PI*x*x*0.01) - M_PI*x*x*0.02 * sin(M_PI*x*x*0.01))) / ro(t, x);
+           - MY * cos(M_TWOPI*t) * M_PI * 0.02 * (cos(M_PI*x*x*0.01) - M_PI*x*x*0.02 * sin(M_PI*x*x*0.01))) /*/ ro(t, x)*/;
 }
 
 void filling_H_0(double *HL, double *HR, double *H, double *HB, double tau, double h, int n)
 {
     HR[0]  = tau * 0.5 * u(h) / h;
     H[0] = 1.0 - tau * 0.5  * 0/*u(0) *// h;
-    HB[0]  = f0(0, 0) + ro(0)
+    HB[0]  = f0(0, 0) * tau + ro(0)
             - (tau * 0.5 / h)
             * ro(0) * (u(h) - 0 /*u(0)*/) + (tau * 0.25 / h) * (2.0 * ro(2*h) * u(2*h)
                                                                    - 2.5 * ro(h) * u(h)
                                                                    + 2.0 * ro(0) * 0 /*u(0)*/
                                                                  - 0.5 * ro(3*h) * u(3*h)
-                                                          + ro(0) * (2.0 * u(2*h) - 2.5 * u(h) - 0.5 * ro(3*h)));
+                                                          + ro(0) * (2.0 * u(2*h) - 2.5 * u(h) - 0.5 * ro(3*h))) * h;
     for (int j = 1; j < n - 1; j++)
     {
         H[j]    = 1.0;
         HR[j]   = (0.25 * tau / h) * (u(j*h) + u((j+1)*h));
         HL[j-1] = - (0.25 * tau / h) * (u(j*h) + u((j-1)*h));
-        HB[j]    = f0(0, j*h) + ro(j*h) * (1.0 - (0.25 * tau / h) * u((j+1)*h) - u((j-1)*h));
+        HB[j]    = f0(0, j*h) * tau + ro(j*h) * (1.0 - (0.25 * tau / h) * (u((j+1)*h) - u((j-1)*h)));
     }
 
     H[n-1]  = 1.0 + (tau * 0.5 / h) * 0/*u((n-1)*h)*/;
     HL[n-2] = - (0.5 * tau / h) * u((n-2)*h);
-    HB[n-1]  = f0(0, (n-1)*h) + ro((n-1)*h) - (0.5 * tau / h) * ro((n-1)*h) * (/*u((n-1)*h)*/0 - u((n-2)*h))
+    HB[n-1]  = f0(0, (n-1)*h) * tau + ro((n-1)*h) - (0.5 * tau / h) * ro((n-1)*h) * (/*u((n-1)*h)*/0 - u((n-2)*h))
             - (0.25 * tau / h) * (2.0 * ro((n-1)*h)  * 0/*u((n-1)*h)*/ - 2.5 * ro((n-2)*h) * u((n-2)*h)
                                 + 2.0 * ro((n-3)*h) * u((n-3)*h) - 0.5 * ro((n-4)*h) * u((n-4)*h)
-                                + ro((n-1)*h) * (u((n-3)*h) - 2.5 * u((n-2)*h) - 0.5 * u((n-4)*h)));
+                                + ro((n-1)*h) * (u((n-3)*h) - 2.5 * u((n-2)*h) - 0.5 * u((n-4)*h))) * h;
 }
 
 /*void filling_H(double *HL, double *HR, double *H, double *HB, double tau, double h, int n, int i)
@@ -179,28 +179,28 @@ void filling_H(double *HL, double *HR, double *H, double *HB, double tau, double
 
     HR[0]  = tau * 0.5 * u(i_tau, h) / h;
     H[0] = 1.0 - tau * 0.5 * u(i_tau, 0) / h;
-    HB[0]  = f0(i_tau, 0) + ro(i_tau, 0)
+    HB[0]  = f0(i_tau, 0) * tau + ro(i_tau, 0)
             - (tau * 0.5 / h)
             * ro(i_tau, 0) * (u(i_tau, h) - u(i_tau, 0)) + (tau * 0.25 / h) * (2.0 * ro(i_tau, 2*h) * u(i_tau, 2*h)
                                                                    - 2.5 * ro(i_tau, h) * u(i_tau, h)
                                                                    + 2.0 * ro(i_tau, 0) * u(i_tau, 0)
                                                                  - 0.5 * ro(i_tau, 3*h) * u(i_tau, 3*h)
-                                                          + ro(i_tau, 0) * (2.0 * u(i_tau, 2*h) - 2.5 * u(i_tau, h) - 0.5 * ro(i_tau, 3*h)));
+                                                          + ro(i_tau, 0) * (2.0 * u(i_tau, 2*h) - 2.5 * u(i_tau, h) - 0.5 * ro(i_tau, 3*h))) /* * h*/;
 
     //double HM = HB[n-1], HM_1 = HB[n-2], HM_2 = HB[n-3], HM_3 = HB[n-4];
 
     for (int j = 1; j < n - 1; j++)
     {
-        HB[j] = f0(i_tau, j*h) + ro(i_tau, j*h) * (1.0 - (0.25 * tau / h) * (u(i_tau, (j+1)*h) - u(i_tau, (j-1)*h)));
+        HB[j] = f0(i_tau, j*h) * tau + ro(i_tau, j*h) * (1.0 - (0.25 * tau / h) * (u(i_tau, (j+1)*h) - u(i_tau, (j-1)*h)));
         H[j]  = 1.0;
         HR[j] = (0.25 * tau / h) * (u(i_tau, j*h) + u(i_tau, (j+1)*h));
         HL[j-1] = - (0.25 * tau / h) * (u(i_tau, j*h) + u(i_tau, (j-1)*h));
     }
 
-    HB[n-1] = f0(i_tau, (n-1)*h) + ro(i_tau, (n-1)*h) - (0.5 * tau / h) * ro(i_tau, (n-1)*h) * (u(i_tau, (n-1)*h)- u(i_tau, (n-2)*h))
+    HB[n-1] = f0(i_tau, (n-1)*h) * tau + ro(i_tau, (n-1)*h) - (0.5 * tau / h) * ro(i_tau, (n-1)*h) * (u(i_tau, (n-1)*h)- u(i_tau, (n-2)*h))
             - (0.25 * tau / h) * (2.0 * ro(i_tau, (n-1)*h) * u(i_tau, (n-1)*h) - 2.5 * ro(i_tau, (n-2)*h) * u(i_tau, (n-2)*h)
                                   + 2.0 * ro(i_tau, (n-3)*h) * u(i_tau, (n-3)*h) - 0.5 * ro(i_tau, (n-4)*h) * u(i_tau, (n-4)*h)
-                                  + ro(i_tau, (n-1)*h) * (-0.5 * u(i_tau, (n-4)*h) - 2.5 * u(i_tau, (n-2)*h) + u(i_tau, (n-3)*h)));
+                                  + ro(i_tau, (n-1)*h) * (-0.5 * u(i_tau, (n-4)*h) - 2.5 * u(i_tau, (n-2)*h) + u(i_tau, (n-3)*h)))/* * h*/;
     H[n-1]  = 1.0 + (0.5 * tau / h) * u(i_tau, (n-1)*h);
     HL[n-2] = - (0.5 * tau / h) * u(i_tau, (n-2)*h);
 }
@@ -222,22 +222,22 @@ void filling_V_0(double *VL, double *V, double *VR, double *VB, double tau, doub
 
     VR[0] = tau / (6.0 * h) * (u(h) + u(2*h)) - mu * tau / (h * h);
     V[0]  = 1.0 + tau * 2.0 * mu / (h * h);
-    VB[1] = u(h) - (pow(ro(tau, 2*h), 1.4) - pow(ro(tau, 0), 1.4)) / (2.0 * h * ro(tau, h))
-            - (mu - MY/ro(tau, h)) * (u(2*h) - 2 * u(h) + u(0)) / (h * h) + f(0, h);
+    VB[1] = u(h) - tau * (pow(ro(tau, 2*h), 1.4) - pow(ro(tau, 0), 1.4)) / (2.0 * h * ro(tau, h))
+            - tau * (mu - MY/ro(tau, h)) * (u(2*h) - 2 * u(h) + u(0)) / (h * h) + f(0, h)*tau;
 
     for (int i = 1; i < n-3; i++)
     {
         V[i]    = 1.0 + tau * 2.0 * mu / (h * h);
-        VR[i]   =   tau / (6.0 * h) * (u((i+1) * h) + u((i+2) * h)) - mu * tau / (h * h);
-        VL[i-1] = - tau / (6.0 * h) * (u((i+1) * h) + u(i * h)) - mu * tau / (h * h);
-        VB[i+1] = u((i+1) * h) - (pow(ro(tau, (i+2)*h), 1.4) - pow(ro(tau, i*h), 1.4)) / (2.0 * h * ro(tau, (i+1)*h))
-                - (mu - MY/ro(tau, (i+1)*h)) * (u((i+2)*h) - 2 * u((i+1)*h) + u(i*h)) / (h * h) + f(0, (i+1)*h);
+        VR[i]   =   (tau / (6.0 * h)) * (u((i+1) * h) + u((i+2) * h)) - mu * tau / (h * h);
+        VL[i-1] = - (tau / (6.0 * h)) * (u((i+1) * h) + u(i * h)) - mu * tau / (h * h);
+        VB[i+1] = u((i+1) * h) - tau * (pow(ro(tau, (i+2)*h), 1.4) - pow(ro(tau, i*h), 1.4)) / (2.0 * h * ro(tau, (i+1)*h))
+                - tau * (mu - MY/ro(tau, (i+1)*h)) * (u((i+2)*h) - 2 * u((i+1)*h) + u(i*h)) / (h * h) + f(0, (i+1)*h)*tau;
     }
 
      V [n-3] = 1.0 + tau * 2.0 * mu / (h * h);
-     VL[n-4] = - tau / (6.0 * h) * (u((n-2)*h) + u((n-3)*h)) - mu * tau / (h * h);
-     VB[n-2] = u((n-2) * h) - (pow(ro(tau, (n-1)*h), 1.4) - pow(ro(tau, (n-3)*h), 1.4)) / (2.0 * h * ro(tau, (n-2)*h))
-             - (mu - MY/ro(tau, (n-2)*h)) * (u((n-1)*h) - 2 * u((n-2)*h) + u((n-3)*h)) / (h * h) + f(0, (n-2)*h);
+     VL[n-4] = - (tau / (6.0 * h)) * (u((n-2)*h) + u((n-3)*h)) - mu * tau / (h * h);
+     VB[n-2] = u((n-2) * h) - tau * (pow(ro(tau, (n-1)*h), 1.4) - pow(ro(tau, (n-3)*h), 1.4)) / (2.0 * h * ro(tau, (n-2)*h))
+             - tau * (mu - MY/ro(tau, (n-2)*h)) * (u((n-1)*h) - 2 * u((n-2)*h) + u((n-3)*h)) / (h * h) + f(0, (n-2)*h)*tau;
 }
 
 /*void filling_V(double *VL, double *V, double *VR, double *VB, double tau, double h, int n, int j)
@@ -309,32 +309,32 @@ void filling_V(double *VL, double *V, double *VR, double *VB, double tau, double
     VB[0]   = 0.0;
     VB[n-1] = 0.0;
 
-    VR[0] = tau / (6.0 * h) * (u(j_tau, h) + u(j_tau, 2*h)) - mu * tau / (h * h);
+    VR[0] = (tau / (6.0 * h)) * (u(j_tau, h) + u(j_tau, 2*h)) - mu * tau / (h * h);
     V[0]  = 1.0 + tau * 2.0 * mu / (h * h);
     //prev_VB_curr = VB[1];
     //prev_VB_prev = VB[0];
-    VB[1] = u(j_tau, h) - (pow(ro(j_1_tau, 2*h), 1.4) - pow(ro(j_1_tau, 0), 1.4)) / (2.0 * h * ro(j_1_tau, h))
-            - (mu - MY/ro(j_1_tau, h)) * (u(j_tau, 2*h) - 2 * u(j_tau, h) + u(j_tau, 0)) / (h * h) + f(j_tau, h);
+    VB[1] = u(j_tau, h) - tau * (pow(ro(j_1_tau, 2*h), 1.4) - pow(ro(j_1_tau, 0), 1.4)) / (2.0 * h * ro(j_1_tau, h))
+            - tau * (mu - MY/ro(j_1_tau, h)) * (u(j_tau, 2*h) - 2 * u(j_tau, h) + u(j_tau, 0)) / (h * h) + f(j_tau, h)*tau;
 
     for (int i = 1; i < n-3; i++)
     {   //prev_VB_prev = prev_VB_curr;
         //prev_VB_curr = VB[i+1];
 
         V[i]    = 1.0 + tau * 2.0 * mu / (h * h);
-        VR[i]   =   tau / (6.0 * h) * (u(j_tau, (i+1)*h) + u(j_tau, (i+2)*h)) - mu * tau / (h * h);
-        VL[i-1] = - tau / (6.0 * h) * (u(j_tau, (i+1)*h) + u(j_tau, i*h)) - mu * tau / (h * h);
+        VR[i]   =   (tau / (6.0 * h)) * (u(j_tau, (i+1)*h) + u(j_tau, (i+2)*h)) - mu * tau / (h * h);
+        VL[i-1] = - (tau / (6.0 * h)) * (u(j_tau, (i+1)*h) + u(j_tau, i*h)) - mu * tau / (h * h);
 
-        VB[i+1] = u(j_tau, (i+1)*h) - (pow(ro(j_1_tau, (i+2)*h), 1.4) - pow(ro(j_1_tau, i*h), 1.4)) / (2.0 * h * ro(j_1_tau, (i+1)*h))
-                - (mu - MY/ro(j_1_tau, (i+1)*h)) * (u(j_tau, (i+2)*h) - 2 * u(j_tau, (i+1)*h) + u(j_tau, i*h)) / (h * h) + f(j_tau, (i+1)*h);
+        VB[i+1] = u(j_tau, (i+1)*h) - tau * (pow(ro(j_1_tau, (i+2)*h), 1.4) - pow(ro(j_1_tau, i*h), 1.4)) / (2.0 * h * ro(j_1_tau, (i+1)*h))
+                - tau * (mu - MY/ro(j_1_tau, (i+1)*h)) * (u(j_tau, (i+2)*h) - 2 * u(j_tau, (i+1)*h) + u(j_tau, i*h)) / (h * h) + f(j_tau, (i+1)*h)*tau;
     }
 
    //prev_VB_prev = prev_VB_curr;
     //prev_VB_curr = VB[n-2];
 
      V [n-3] = 1.0 + tau * 2.0 * mu / (h * h);
-     VL[n-4] = - tau / (6.0 * h) * (u(j_tau, (n-2)*h) + u(j_tau, (n-3)*h)) - mu * tau / (h * h);
-     VB[n-2] = u(j_tau, (n-2)*h) - (pow(ro(j_1_tau, (n-1)*h), 1.4) - pow(ro(j_1_tau, (n-3)*h), 1.4)) / (2.0 * h * ro(j_1_tau, (n-2)*h))
-             - (mu - MY/ro(j_1_tau, (n-2)*h)) * (u(j_tau, (n-1)*h) - 2 * u(j_tau, (n-2)*h) + u(j_tau, (n-3)*h)) / (h * h) + f(j_tau, (n-2)*h);
+     VL[n-4] = - (tau / (6.0 * h)) * (u(j_tau, (n-2)*h) + u(j_tau, (n-3)*h)) - mu * tau / (h * h);
+     VB[n-2] = u(j_tau, (n-2)*h) - tau * (pow(ro(j_1_tau, (n-1)*h), 1.4) - pow(ro(j_1_tau, (n-3)*h), 1.4)) / (2.0 * h * ro(j_1_tau, (n-2)*h))
+             - tau * (mu - MY/ro(j_1_tau, (n-2)*h)) * (u(j_tau, (n-1)*h) - 2 * u(j_tau, (n-2)*h) + u(j_tau, (n-3)*h)) / (h * h) + f(j_tau, (n-2)*h)*tau;
 }
 
 void calculate(double *H, double *HB, double *HL, double *HR, int n, int m, double h, double tau,
@@ -342,20 +342,20 @@ void calculate(double *H, double *HB, double *HL, double *HR, int n, int m, doub
 {
     filling_H_0(HL, HR, H, HB, tau, h, n);
     ThreeDiagSolve(HB, H, HR, HL, n);
-    for (int i = 0; i < n; i++)
+    /*for (int i = 0; i < n; i++)
     {
         cout << "HB[" << i <<"] = " << HB[i] << " ro = " << ro(1*tau, i*h) << endl;
-    }
-    cout << endl;
+    }*/
+    //cout << endl;
     for (int i = 1; i < m-1; i++)
     {
         filling_H(HL, HR, H, HB, tau, h, n, i);
         ThreeDiagSolve(HB, H, HR, HL, n);
-        for (int j = 0; j < n; j++)
+        /*for (int j = 0; j < n; j++)
         {
             cout << "HB[" << j <<"] = " << HB[j] << " ro = " << ro((i+1)*tau, j*h) << endl;
         }
-        cout << endl;
+        cout << endl;*/
 
     }
 
@@ -368,9 +368,9 @@ void calculate(double *H, double *HB, double *HL, double *HR, int n, int m, doub
         if (diff > res)
             res = diff;
     }
-    cout << "\nResidual is " << res << endl << endl;
+    cout << "\nResidual H is " << res << endl << endl;
 
-    /*filling_V_0(VL, V, VR, VB, tau, h, n);
+    filling_V_0(VL, V, VR, VB, tau, h, n);
     ThreeDiagSolve(VB+1, V, VR, VL, n-2);
 
     for (int i = 1; i < m-1; i++)
@@ -387,7 +387,7 @@ void calculate(double *H, double *HB, double *HL, double *HR, int n, int m, doub
         if (diff > res)
         res = diff;
     }
-    cout << "\nResidual is " << res << endl << endl;*/
+    cout << "\nResidual V is " << res << endl << endl;
 }
 
 
